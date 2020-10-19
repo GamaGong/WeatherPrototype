5966d401d97b7e031f0d192ce956518ef752783d
Evgenii_Zubarev <evgenii_zubarev@epam.com>
2020-10-19 16:05:39 +0300

    add viewbinding sugar

--- app/src/main/java/com/example/weatherprototype/MainActivity.kt
+++ app/src/main/java/com/example/weatherprototype/MainActivity.kt
@@ -1,23 +1,10 @@
 package com.example.weatherprototype
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.dropbox.android.external.store4.fresh
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.weatherprototype.databinding.ActivityMainBinding
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    @ExperimentalCoroutinesApi
-    @FlowPreview
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
+    private val viewBinding by viewBinding(ActivityMainBinding::bind, R.id.container)
 }
